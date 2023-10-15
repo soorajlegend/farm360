750b2851350bdde963de2d61e2f842a5f1318cbd
suraj muhammad <soorajwizard01@gmail.com>
2023-10-15 17:07:47 +0300

    added scroll to bottom for chat body

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -8,7 +8,7 @@ interface ChatHeaderProps {
 
 const ChatHeader = ({ onClose }: ChatHeaderProps) => {
     return (
-        <div className='flex items-center w-full gap-3 bg-slate-100 py-2 px-4'>
+        <div className='flex items-center w-full gap-3 bg-slate-50 py-2 px-4'>
             <div className="flex gap-3 items-center">
                 <Avatar>
                     <AvatarImage src='/x.png' />
--- components/chat/chat.tsx
+++ components/chat/chat.tsx
@@ -13,11 +13,11 @@ const Chat = ({ isOpen, onClose }: ChatProps) => {
 
     return (
         <div className={cn(
-            "fixed  bottom-0 left-0 w-full h-full max-w-xl max-h-[92vh] bg-white shadow-md rounded-md z-50 transition duration-500 flex flex-col",
+            "fixed  bottom-0 left-0 w-full h-full max-w-xl max-h-[93vh] bg-white shadow-md rounded-md z-50 transition duration-500 flex flex-col",
             isOpen ? "-translate-x-0 translate-y-0" : "-translate-x-full translate-y-full"
         )}>
             <ChatHeader onClose={onClose} />
-            <ChatBody />
+            <ChatBody isOpen={isOpen} />
             <ChatInput />
         </div>
     )
